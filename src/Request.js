c3c02169f9fd47ad1f76e909e4a3940f8461c0f3
Faid Terence JABO <faidterence@outlook.com>
2023-04-14 17:35:57 +0200

    Stunna flix app built with react

---
+++ src/Request.js
@@ -0,0 +1,12 @@
+const key = '01ea218f3d3b4cfbfa2e7514431b5f6b'
+
+export const requests = {
+    requestPopular: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=1`,
+    requestTopRated: `https://api.themoviedb.org/3/movie/top_rated?api_key=${key}&language=en-US&page=1`,
+    requestTrending: `https://api.themoviedb.org/3/movie/popular?api_key=${key}&language=en-US&page=2`,
+    requestHorror: `https://api.themoviedb.org/3/search/movie?api_key=${key}&language=en-US&query=horror&page=1&include_adult=false`,
+    requestUpcoming: `https://api.themoviedb.org/3/movie/upcoming?api_key=${key}&language=en-US&page=1`,
+  };
+
+  
+
